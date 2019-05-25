2ad753b3ba8016efbe90968b0d875c0b5a3647b7
Teekumi <le.david993@gmail.com>
2019-05-25 17:33:41 -0500

    added cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const keys = require("./config/keys");
 const passport = require("passport");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require('cors')
 require("./models/User");
 require("./models/Novel");
 require("./models/Chapter")
@@ -12,6 +13,7 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(
     cookieSession({
